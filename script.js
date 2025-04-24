ca0a1fd12f2b5590a4ab365e358261f2752483b6
Selvaganesh.S <130886211+Selvaganesh2244@users.noreply.github.com>
2025-04-24 19:52:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let menuIcon = document.querySelector('#menu-icon');
+let nav = document.querySelector('.nav');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+};
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -16,4 +24,10 @@ window.onscroll = () => {
             document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
         }
     });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    nav.classList.remove('active');
 };
